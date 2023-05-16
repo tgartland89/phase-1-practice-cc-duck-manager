734c0d1b783ca47f2e103ca87e19166e6fa669ba
tgartland89 <thomas.gartland@gmail.com>
2023-05-15 21:51:03 -0600

    added function to createDuckElement for likes button

--- src/index.js
+++ src/index.js
@@ -1,48 +1,54 @@
 document.addEventListener('DOMContentLoaded', function() {
-  const duckNav = document.querySelector('#duck-nav');
-  const duckDisplay = document.querySelector('#duck-display');
-  const duckNameElement = document.querySelector('#duck-display-name');
-  const duckImageElement = document.querySelector('#duck-display-image');
-  const likesCountElement = document.querySelector('#duck-display-likes');
+  let duckNav = document.querySelector('#duck-nav');
+  let duckDisplay = document.querySelector('#duck-display');
+  let duckNameElement = document.querySelector('#duck-display-name');
+  let duckImageElement = document.querySelector('#duck-display-image');
+  let likesCountElement = document.querySelector('#duck-display-likes');
+  let newDuckForm = document.querySelector('#new-duck-form');
 
   fetch('http://localhost:3000/ducks')
     .then(response => response.json())
     .then(data => {
       data.forEach(duck => {
-        const img = document.createElement('img');
-        img.src = duck.img_url;
-        img.alt = duck.name;
-        duckNav.appendChild(img);
-
-        img.addEventListener('click', () => {
-          duckNameElement.textContent = duck.name;
-          duckImageElement.src = duck.img_url;
-          duckImageElement.alt = duck.name;
-          likesCountElement.textContent = duck.likes;
-          duckDisplay.style.display = 'block';
-        });
+        createDuckElement(duck);
       });
 
-      const likesButton = document.querySelector('#duck-display-likes');
-      likesButton.addEventListener('click', () => {
-        const currentLikes = parseInt(likesCountElement.textContent);
-        // const duckIndex = duckNav.querySelector('.active').dataset.index;
-        // data[duckIndex].likes = currentLikes + 1;
-        likesCountElement.textContent = currentLikes + 1;
+      newDuckForm.addEventListener('submit', event => {
+        event.preventDefault();
+        let newDuckNameInput = document.querySelector('input[name="duck-name-input"]');
+        let newDuckImageInput = document.querySelector('input[name="duck-image-input"]');
+        let newDuckName = newDuckNameInput.value;
+        let newDuckImageUrl = newDuckImageInput.value;
+        let newDuck = {
+          name: newDuckName,
+          img_url: newDuckImageUrl,
+          likes: 0
+        };
+
+        createDuckElement(newDuck);
+        newDuckNameInput.value = '';
+        newDuckImageInput.value = '';
       });
     });
+
+  function createDuckElement(duck) {
+    let img = document.createElement('img');
+    img.src = duck.img_url;
+    img.alt = duck.name;
+    duckNav.appendChild(img);
+
+    img.addEventListener('click', () => {
+      duckNameElement.textContent = duck.name;
+      duckImageElement.src = duck.img_url;
+      duckImageElement.alt = duck.name;
+      likesCountElement.textContent = duck.likes;
+      duckDisplay.style.display = 'block';
+    });
+
+    let likesButton = document.querySelector('#duck-display-likes');
+    likesButton.addEventListener('click', () => {
+      duck.likes += 1;
+      likesCountElement.textContent = `${duck.likes} likes`;
+    });
+  }
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
