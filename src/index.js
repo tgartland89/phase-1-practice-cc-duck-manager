7c576c2267887194a9942573d245d278dbc5262c
tgartland89 <thomas.gartland@gmail.com>
2023-05-15 20:32:20 -0600

    added ducks to bar

--- src/index.js
+++ src/index.js
@@ -1,1 +1,44 @@
-// write your code here!
+document.addEventListener('DOMContentLoaded', function() {
+  const duckNav = document.querySelector('#duck-nav');
+  const duckDisplay = document.querySelector('#duck-display');
+  const duckNameElement = document.querySelector('#duck-display-name');
+  const duckImageElement = document.querySelector('#duck-display-image');
+  const likesCountElement = document.querySelector('#duck-display-likes');
+
+  fetch('http://localhost:3000/ducks')
+    .then(response => response.json())
+    .then(data => {
+      data.forEach(duck => {
+        const img = document.createElement('img');
+        img.src = duck.img_url;
+        img.alt = duck.name;
+        duckNav.appendChild(img);
+        
+      });
+    })
+});  fetch('http://localhost:3000/ducks')
+.then(response => response.json())
+.then(data => {
+  data.forEach(duck => {
+    const img = document.createElement('img');
+    img.src = duck.img_url;
+    img.alt = duck.name;
+    duckNav.appendChild(img);
+
+    img.addEventListener('click', () => {
+      duckNameElement.textContent = duck.name;
+      duckImageElement.src = duck.img_url;
+      duckImageElement.alt = duck.name;
+      likesCountElement.textContent = duck.likes;
+      duckDisplay.style.display = 'block';
+    });
+  });
+})
+
+
+
+
+
+
+
+
